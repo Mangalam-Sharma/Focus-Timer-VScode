c129ab0ecd9bfd0e610b8c5c81e0afe1a882ac25
Mangalam Sharma <mangalamsharma2050@gmail.com>
2022-10-10 20:07:23 +0530

    make it better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # This is a sample Python script.
-#this ia a change
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
